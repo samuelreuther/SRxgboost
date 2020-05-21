4157681baaff6970d15027bf06d7255b23a9a1b7
rossam611 <samuel.reuther@web.de>
2020-05-21 22:38:01 +0200

    delete metrics

--- tests/testthat/test-SRxgboost_f1.R
+++
@@ -1,5 +0,0 @@
-test_that("result", {
-  expect_equal(SRxgboost_f1(pred = seq(0.1, 1, 0.1),
-                            labels = c(1, 0, 0, 0, 1, 1, 0, 1, 1, 1)),
-               list(f1 = 0.83333, opt_cutoff = 0.5))
-})
--- tests/testthat/test-SRxgboost_mcc.R
+++
@@ -1,5 +0,0 @@
-test_that("result", {
-  expect_equal(SRxgboost_mcc(pred = seq(0.1, 1, 0.1),
-                             labels = c(1, 0, 0, 0, 1, 1, 0, 1, 1, 1)),
-               list(mcc = 0.58333, opt_cutoff = 0.5))
-})
