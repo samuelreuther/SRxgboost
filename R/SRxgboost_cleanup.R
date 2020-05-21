0ee3427c3bfe876debe52aee10c43a534159150f
rossam611 <samuel.reuther@web.de>
2020-05-22 00:14:55 +0200

    SRxgboost_cleanup

---
+++ R/SRxgboost_cleanup.R
@@ -0,0 +1,23 @@
+#' SRxgboost_cleanup
+#'
+#' Removes all standard SRxgboost-objects from global environment.
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
+SRxgboost_cleanup <- function() {
+  suppressWarnings(
+    try(rm(datenModell, SummaryCV, train_pr_oof, xgbfi, bst, bst_1fold, dtesting, dtrain,
+           dtraining, featureList, lauf, lf, no_folds, res, trainmat, y, data_train,
+           datenModelltest, testing, training, data_test, objective, testingmat, trainingmat,
+           yname, ytesting, ytraining, testmat, importance_matrix, eval_index,
+           nround, eta, obj, metric, runs, nfold, trees, verbose, test_param, save_model,
+           run_kfold, save_test_forecast, best_params, d_test, d_test_eval, d_train,
+           d_train_eval, test_eval_mat, test_mat, train_eval_mat, train_mat, y_test_eval,
+           y_train_eval, factor_encoding, forecast_mat, d_forecast, datenModell_eval,
+           datenModell_train, y_test, folds, y_train, y_train_all,
+           envir = .GlobalEnv), TRUE))
+  invisible(gc())
+}
