e9decf75cfc1fc9f41ebecf07de6ef9c3cbed70f
rossam611 <samuel.reuther@web.de>
2020-05-21 23:55:14 +0200

    SRxgboost_create_folds

---
+++ R/SRxgboost_create_folds.R
@@ -0,0 +1,39 @@
+#' SRxgboost_create_folds
+#'
+#' Creates custom folds for 'SRxgboost_run(folds = ...)', for example time based,
+#' groups, ...
+#'
+#' @param df data.frame
+#' @param foldcolumn character
+#' @param k integer
+#'
+#' @return list
+#'
+#' @examples
+#'
+#' @export
+SRxgboost_create_folds <- function(df,
+                                   foldcolumn,
+                                   k = 5) {
+  # convert foldcolumn as.factor
+  if (class(df[, foldcolumn]) != "factor") {
+    df[, foldcolumn] <- as.factor(df[, foldcolumn])
+  }
+  #
+  # create fold index in df
+  set.seed(12345)
+  df <- groupdata2::fold(df, k = k, id_col = foldcolumn) %>%
+    dplyr::select(folds = .folds, dplyr::everything()) %>%
+    data.frame()
+  set.seed(Sys.time())
+  #
+  # create list of folds indices for XGBOOST
+  fold_ids <- unique(as.numeric(df[, "folds"]))
+  folds <- vector("list", dplyr::n_distinct(df[, "folds"]))
+  for (fold_id in fold_ids) {
+    folds[[fold_id]] <- which(as.numeric(df[, "folds"]) %in% fold_id)
+  }
+  #
+  # return folds
+  return(folds)
+}
