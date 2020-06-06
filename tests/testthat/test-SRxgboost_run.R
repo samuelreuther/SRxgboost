bf70e44ef00f77287881c83b9d95aba199ba61ea
Samuel Reuther <samuel.reuther@helvetia.ch>
2020-06-06 14:02:21 +0000

    added run-tests for classification

--- tests/testthat/test-SRxgboost_run.R
+++ tests/testthat/test-SRxgboost_run.R
@@ -142,8 +142,11 @@ test_that("regression / eval_index: runtime[2]", {
 })
 # rmse
 test_that("regression / eval_index: SummaryCV$eval_1fold[1]", {
-  expect_equal(round(SummaryCV$eval_1fold[1], -3), 21000)
+  expect_true(SummaryCV$eval_1fold[1] < 21000)
+  # expect_equal(round(SummaryCV$eval_1fold[1], -3), 21000)
 })
+# assert(paste0("\n SummaryCV$eval_1fold[1] = ", SummaryCV$eval_1fold[1]),
+#        SummaryCV$eval_1fold[1] > 20000 & SummaryCV$eval_1fold[1] < 21000)
 #
 #
 ## clean up
@@ -218,6 +221,211 @@ SRxgboost_cleanup()
 # Regression: clean up ----------------------------------------------------
 #
 rm(house, train, id_unique_train)
+
+
+
+
+# Classification: read data -----------------------------------------------
+#
+churn <- utils::read.csv(paste0(path_to_data,
+                                "Classification/Telco Customer Churn/Telco-Customer-Churn.csv"))
+assign('churn', churn, envir = .GlobalEnv)
+id_unique_train <- churn$customerID
+assign('id_unique_train', id_unique_train, envir = .GlobalEnv)
+train <- churn %>%
+  mutate(Churn = case_when(Churn == "No"  ~ 0,
+                           Churn == "Yes" ~ 1,
+                           T              ~ NA_real_)) %>%
+  select(-customerID)
+assign('train', train, envir = .GlobalEnv)
+
+
+
+
+# Classification: no_folds ------------------------------------------------
+#
+## run models
+#
+lauf <- "SRxgboost_test_classification_no_folds.csv"
+assign('lauf', lauf, envir = .GlobalEnv)
+# prepare data and test
+SRxgboost_data_prep(yname = "Churn",
+                    data_train = train,
+                    no_folds = 5,
+                    objective = "binary")
+# run models
+SRxgboost_run(nround = 1000, eta = 0.1, obj = "binary:logistic", metric = "auc", runs = 2,
+              nfold = 5)
+#
+#
+## tests
+#
+# no. of files
+test_that("classification / no_folds: files in path_output/lauf", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/"))), 7)
+})
+test_that("classification / no_folds: files in path_output/lauf/All Models", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/All Models"))), 10)
+})
+# test_that("classification / no_folds: files in path_output/lauf/Best Model", {
+#   expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Best Model"))), 30)
+# })
+test_that("classification / no_folds: files in path_output/lauf/Data", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Data"))), 18)
+})
+# runtime
+test_that("classification / no_folds: runtime[1]", {
+  expect_true(SummaryCV$runtime[1] < 0.2)
+})
+test_that("classification / no_folds: runtime[2]", {
+  expect_true(SummaryCV$runtime[2] < 0.1)
+})
+# auc
+test_that("classification / no_folds: SummaryCV$eval_1fold[1]", {
+  expect_equal(round(SummaryCV$eval_1fold[1], 2), 0.84)
+})
+test_that("classification / no_folds: SummaryCV$train[1]", {
+  expect_equal(round(SummaryCV$train[1], 2), 0.92)
+})
+test_that("classification / no_folds: SummaryCV$test[1]", {
+  expect_equal(round(SummaryCV$test[1], 2), 0.84)
+})
+#
+#
+## clean up
+#
+SRxgboost_cleanup()
+
+
+
+
+# Classification: eval_index ----------------------------------------------
+#
+## run models
+#
+lauf <- "SRxgboost_test_classification_eval_index.csv"
+assign('lauf', lauf, envir = .GlobalEnv)
+# create eval_index
+eval_index <- which(train$MonthlyCharges > 100)
+assign('eval_index', eval_index, envir = .GlobalEnv)
+# prepare data and test
+SRxgboost_data_prep(yname = "Churn",
+                    data_train = train,
+                    eval_index = eval_index,
+                    objective = "binary")
+# run models
+SRxgboost_run(nround = 1000, eta = 0.1, obj = "binary:logistic", metric = "auc", runs = 2,
+              run_final_model = FALSE)
+#
+#
+## tests
+#
+# no. of files
+test_that("classification / eval_index: files in path_output/lauf", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/"))), 6)
+})
+test_that("classification / eval_index: files in path_output/lauf/All Models", {
+  expect_true(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/All Models"))) %in% c(8, 16))
+})
+# test_that("classification / eval_index: files in path_output/lauf/Best Model", {
+#   expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Best Model"))), 30)
+# })
+# assert(paste0("\n files in path_output/lauf/Best Model = ",
+#               length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Best Model")))),
+#        length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Best Model"))) ==
+#          read.csv2(paste0(path_output, gsub(".csv", "", lauf),
+#                           "/Best Model/0 Variable importance.csv")) %>%
+#          filter(Gain >= 0.01) %>%
+#          nrow() * 2 + 12)
+test_that("classification / eval_index: files in path_output/lauf/Data", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Data"))), 19)
+})
+# runtime
+test_that("classification / eval_index: runtime[1]", {
+  expect_true(SummaryCV$runtime[1] < 0.1)
+})
+test_that("classification / eval_index: runtime[2]", {
+  expect_true(SummaryCV$runtime[2] < 0.1)
+})
+# auc
+test_that("classification / eval_index: SummaryCV$eval_1fold[1]", {
+  expect_true(SummaryCV$eval_1fold[1] > 0.77)
+  # expect_equal(round(SummaryCV$eval_1fold[1], 2), 0.77)
+})
+#
+#
+## clean up
+#
+SRxgboost_cleanup()
+
+
+
+
+# Classification: folds ---------------------------------------------------
+#
+## run models
+#
+lauf <- "SRxgboost_test_classification_folds.csv"
+assign('lauf', lauf, envir = .GlobalEnv)
+# create folds
+train$group <- c(1, 1, 1, rep(1:(nrow(train) / 10), each = 10))
+folds <- SRxgboost_create_folds(df = train, foldcolumn = "group", k = 5)
+assign('folds', folds, envir = .GlobalEnv)
+train <- train %>% dplyr::select(-group)
+# prepare data and test
+SRxgboost_data_prep(yname = "Churn",
+                    data_train = train,
+                    folds = folds,
+                    objective = "binary")
+# run models
+SRxgboost_run(nround = 1000, eta = 0.1, obj = "binary:logistic", metric = "auc", runs = 2,
+              folds = folds)
+#
+#
+## tests
+#
+# no. of files
+test_that("classification / folds: files in path_output/lauf", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/"))), 7)
+})
+test_that("classification / folds: files in path_output/lauf/All Models", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/All Models"))), 10)
+})
+# test_that("classification / folds: files in path_output/lauf/Best Model", {
+#   expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Best Model"))), 30)
+# })
+test_that("classification / folds: files in path_output/lauf/Data", {
+  expect_equal(length(list.files(paste0(path_output, gsub(".csv", "", lauf), "/Data"))), 19)
+})
+# runtime
+test_that("classification / folds: runtime[1]", {
+  expect_true(SummaryCV$runtime[1] < 0.2)
+})
+test_that("classification / folds: runtime[2]", {
+  expect_true(SummaryCV$runtime[2] < 0.1)
+})
+# auc
+test_that("classification / folds: SummaryCV$eval_1fold[1]", {
+  expect_equal(round(SummaryCV$eval_1fold[1], 2), 0.85)
+})
+test_that("classification / folds: SummaryCV$train[1]", {
+  expect_equal(round(SummaryCV$train[1], 2), 0.90)
+})
+test_that("classification / folds: SummaryCV$test[1]", {
+  expect_equal(round(SummaryCV$test[1], 2), 0.84)
+})
+#
+#
+## clean up
+#
+SRxgboost_cleanup()
+
+
+
+
+# Classification: clean up ----------------------------------------------------
+#
+rm(churn, train, id_unique_train)
 
 
 
