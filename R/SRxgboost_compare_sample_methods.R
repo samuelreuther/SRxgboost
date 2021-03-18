d3a575603698ffbc1553f831d2586cd849f3fd75
Samuel Reuther <samuel.reuther@helvetia.ch>
2021-03-18 22:03:11 +0000

    ...

--- R/SRxgboost_compare_sample_methods.R
+++ R/SRxgboost_compare_sample_methods.R
@@ -41,23 +41,20 @@ SRxgboost_compare_sample_methods <- function(df_train,
       dir.create(path_output, showWarnings = FALSE, recursive = TRUE)
     }
     cat(paste0("\n", "Run:   ", m, "\n"))
-    #
     try({
-      # browser()
       # sample training data
       if (m == "unbalanced") {
-        df_sampled <- df_train
-        folds_sampled <- folds
+        return <- list(df_train, folds)
       } else {
         return <- SRxgboost_sample_training_data(folds = folds, df = df_train,
                                                  sample_method = m, verbose = FALSE)
-        df_sampled <- return[[1]]
-        folds_sampled <- return[[2]]
       }
-      id_unique_train <- 1:nrow(df_sampled)
-      assign("id_unique_train", id_unique_train, envir = .GlobalEnv)
+      df_sampled <- return[[1]]
+      folds_sampled <- return[[2]]
       #
       # run XGB_data_prep
+      id_unique_train <- 1:nrow(df_sampled)
+      assign("id_unique_train", id_unique_train, envir = .GlobalEnv)
       SRxgboost_data_prep(yname = "y", data_train = df_sampled, data_test = df_test,
                           folds = folds_sampled, objective = "classification",
                           check_covariate_drift = FALSE)
@@ -70,35 +67,53 @@ SRxgboost_compare_sample_methods <- function(df_train,
       SRxgboost_plots(lauf = lauf, rank = 1, min_rel_Gain = 0.04)
       #
       # clean up
+      saveRDS(return, paste0(path_output, m, "/Data/return.rds"))
       suppressWarnings(rm(return, df_sampled, folds_sampled, id_unique_train,
-                          OOFforecast, TESTforecast, SummaryCV_META, test_pr, y_OOF))
+                          OOFforecast, TESTforecast, SummaryCV_META, test_pr,
+                          y_OOF, factor_encoding))
       SRxgboost_cleanup()
     }, TRUE)
   }; suppressWarnings(rm(m, lauf))
   #
   ### plot error comparison
   #
-  files <- list.files(path_output, pattern = "Summary.csv", recursive = TRUE)
   comparison <- data.frame()
-  for (i in files) {
-    comparison <- dplyr::bind_rows(comparison,
-                                   utils::read.csv2(paste0(path_output, i)) %>%
-                                     dplyr::arrange(-test) %>%
-                                     utils::head(1) %>%
-                                     dplyr::mutate(Lauf = i %>%
-                                                     gsub("/Summary.csv", "", .)))
-  }; suppressWarnings(rm(i, files))
+  for (i in c("unbalanced", sample_methods)) {
+    return <- readRDS(paste0(path_output, i, "/Data/return.rds"))
+    df_temp <- utils::read.csv2(paste0(path_output, i, "/Summary.csv")) %>%
+      dplyr::arrange(-test) %>%
+      utils::head(1) %>%
+      dplyr::mutate(Lauf = i) %>%
+      mutate(train_nrow = nrow(return[[1]]),
+             train_y_1 = sum(return[[1]]$y == 1),
+             train_y_1_percent = train_y_1 / train_nrow)
+    comparison <- dplyr::bind_rows(comparison, df_temp)
+    rm(return, df_temp)
+  }; suppressWarnings(rm(i))
+  #
+  # files <- list.files(path_output, pattern = "Summary.csv", recursive = TRUE)
+  # comparison <- data.frame()
+  # for (i in files) {
+  #   comparison <- dplyr::bind_rows(comparison,
+  #                                  utils::read.csv2(paste0(path_output, i)) %>%
+  #                                    dplyr::arrange(-test) %>%
+  #                                    utils::head(1) %>%
+  #                                    dplyr::mutate(Lauf = i %>%
+  #                                                    gsub("/Summary.csv", "", .)))
+  # }; suppressWarnings(rm(i, files))
   #
   if (!file.exists(paste0(path_output, "unbalanced/Data/y_test.rds"))) {
-    # plot train and CV results
+    # plot model error
     p <- comparison %>%
       dplyr::select(Lauf, train, CV = test) %>%
       dplyr::mutate(Lauf = factor(Lauf),
                     Lauf = stats::reorder(Lauf, -CV)) %>%
       reshape2::melt(id = "Lauf") %>%
-      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = value, colour = variable, group = variable)) +
+      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = value, colour = variable,
+                                   group = variable, label = round(value, 3))) +
       ggplot2::geom_line(size = 1) +
-      ggplot2::geom_point(size = 3) +
+      ggplot2::geom_point(size = 2) +
+      ggrepel::geom_text_repel(size = 3, direction = "y") +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 0, vjust = 0.5)) +
       ggplot2::labs(y = "AUC", colour = "",
                     title = "Vergleich der Modellgüte der einzelnen Sample Methoden") +
@@ -107,6 +122,27 @@ SRxgboost_compare_sample_methods <- function(df_train,
     print(p)
     ggplot2::ggsave(paste0(path_output, "comparison.png"), p,
                     width = 9.92, height = 5.3)
+    rm(p)
+    #
+    # plot training data
+    p <- comparison %>%
+      dplyr::mutate(Lauf = factor(Lauf),
+                    Lauf = stats::reorder(Lauf, -test)) %>%
+      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = train_nrow, size = train_y_1_percent,
+                                   label = paste0(round(train_y_1_percent, 3) * 100, " %"))) +
+      ggplot2::geom_point() +
+      ggrepel::geom_text_repel(size = 3, direction = "y") +
+      # ggplot2::geom_text(size = 3) +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 0, vjust = 0.5)) +
+      ggplot2::labs(y = "Anzahl Trainingsdaten", size = "Anteil y = 1",
+                    title = "Vergleich der Trainingsdaten der einzelnen Sample Methoden") +
+      ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(6),
+                                  labels = scales::format_format(big.mark = "'")) +
+      ggplot2::scale_size_continuous(labels = scales::percent, limits = c(0, 0.5))
+    print(p)
+    ggplot2::ggsave(paste0(path_output, "comparison_training_data.png"), p,
+                    width = 9.92, height = 5.3)
+    rm(p)
   } else {
     # plot train, test and CV results                      WARNING !!! works only if runs = 2   TODO !!!
     # read y_test
@@ -132,15 +168,17 @@ SRxgboost_compare_sample_methods <- function(df_train,
                                    auc_test %>% dplyr::rename(test_data = auc),
                                    by = "Lauf")
     #
-    # plot
+    # plot model error
     p <- comparison %>%
       dplyr::select(Lauf, train, CV = test, test = test_data) %>%
       dplyr::mutate(Lauf = factor(Lauf),
                     Lauf = stats::reorder(Lauf, -test)) %>%
       reshape2::melt(id = "Lauf") %>%
-      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = value, colour = variable, group = variable)) +
+      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = value, colour = variable,
+                                   group = variable, label = round(value, 3))) +
       ggplot2::geom_line(size = 1) +
-      ggplot2::geom_point(size = 3) +
+      ggplot2::geom_point(size = 2) +
+      ggrepel::geom_text_repel(size = 3, direction = "y") +
       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 0, vjust = 0.5)) +
       ggplot2::labs(y = "AUC", colour = "",
                     title = "Vergleich der Modellgüte der einzelnen Sample Methoden") +
@@ -149,6 +187,26 @@ SRxgboost_compare_sample_methods <- function(df_train,
     print(p)
     ggplot2::ggsave(paste0(path_output, "comparison.png"), p,
                     width = 9.92, height = 5.3)
+    rm(p)
+    #
+    # plot training data
+    p <- comparison %>%
+      dplyr::mutate(Lauf = factor(Lauf),
+                    Lauf = stats::reorder(Lauf, -test_data)) %>%
+      ggplot2::ggplot(ggplot2::aes(x = Lauf, y = train_nrow, size = train_y_1_percent,
+                                   label = paste0(round(train_y_1_percent, 3) * 100, " %"))) +
+      ggplot2::geom_point() +
+      ggrepel::geom_text_repel(size = 3, direction = "y") +
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 0, vjust = 0.5)) +
+      ggplot2::labs(y = "Anzahl Trainingsdaten", size = "Anteil y = 1",
+                    title = "Vergleich der Trainingsdaten der einzelnen Sample Methoden") +
+      ggplot2::scale_y_continuous(breaks = scales::pretty_breaks(6),
+                                  labels = scales::format_format(big.mark = "'")) +
+      ggplot2::scale_size_continuous(labels = scales::percent, limits = c(0, 0.5))
+    print(p)
+    ggplot2::ggsave(paste0(path_output, "comparison_training_data.png"), p,
+                    width = 9.92, height = 5.3)
+    rm(p)
   }
   #
   # save comparison
